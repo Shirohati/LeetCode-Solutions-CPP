58cfec8c9f088a7d5ec635c4787c67a25cbc4849
Shirohati <zxr188721839@163.com>
2026-03-18 22:30:58 +0800

    创建 0088.cpp

---
+++ src/array/0088.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+
+/**
+题目：0088
+给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，
+其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+思路：
+    从后往前遍历
+
+总结：
+    逆向思维
+ */
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(n==0)return;
+        if(m==0){
+            for(int i=0;i<n;i++){
+                nums1[i]=nums2[i];
+            }
+        }
+        int p1=m-1,p2=n-1,j=n+m-1;
+        while(j!=-1){
+            if(p2==-1){
+                while(p1>-1){
+                    nums1[j]=nums1[p1];
+                    p1--;
+                    j--;
+                }
+            }
+            if(p1==-1){
+                while(p2>-1){
+                    nums1[j]=nums2[p2];
+                    p2--;
+                    j--;
+                }
+            }
+            if(p1==-1&&p2==-1)return ;
+            if(nums1[p1]<nums2[p2]){
+                nums1[j]=nums2[p2];
+                p2--;
+                j--;
+            }
+            else{
+                nums1[j]=nums1[p1];
+                p1--;
+                j--;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
